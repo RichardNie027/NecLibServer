463f1f384eb853dd705fa05dc75d9cb54e40f979
RichardNie027 <richard.nie.it@hotmail.com>
2021-03-09 16:52:15 +0800

    add collection util

---
+++ src/main/java/com/nec/lib/utils/CollectionUtil.java
@@ -0,0 +1,10 @@
+package com.nec.lib.utils;
+
+public class CollectionUtil {
+//    public static void removeItemFromList(List<T> list, Object<T> item) {
+//        Iterator<T> it = list.iterator();
+//        while(it.hasNext()) {
+//
+//        }
+//    }
+}
